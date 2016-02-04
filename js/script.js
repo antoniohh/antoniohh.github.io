f9467ae4eddb8c5a0d3728717a0d1aa45bbc86db
antoniohh <antoniohh76@gmail.com>
2016-02-04 22:53:52 +0100

    Nueva página personal

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Carga de Secciones
+$(document).ready(function(){
+    $( '#body' ).load( 'vista/body.html' );
+    $( '#sobremi' ).load( 'vista/sobremi.html' );
+    $( '#formacion' ).load( 'vista/formacion.html' );
+    $( '#portfolio' ).load( 'vista/portfolio.html' );
+    $( '#conocimientos' ).load( 'vista/conocimientos.html' );
+    $( '#experiencia' ).load( 'vista/experiencia.html' );
+    $( '#curriculum' ).load( 'vista/curriculum.html' );
+    $( '#contacto' ).load( 'vista/contacto.html' );
+    $( '#servicios' ).load( 'vista/servicios.html' );
+    $( '#blog' ).load( 'vista/blog.html' );
+    $( '#footer' ).load( 'vista/footer.html' );
+});
