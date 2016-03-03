89df85bc5891dd3f59ba730aaac50688fd721ff9
antoniohh <antoniohh76@gmail.com>
2016-03-03 19:19:33 +0100

    Actualización Contenido.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
-// Carga de Secciones
+/* 
+ * Copyright (C) 2016 Antonio Horrillo Horrillo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Función que relaciona los id´s de los divs con las páginas html con el contenido
+ * de la seccion de la página.
+ * @param {type} param
+ */
 $(document).ready(function(){
     $( '#body' ).load( 'vista/body.html' );
     $( '#sobremi' ).load( 'vista/sobremi.html' );
